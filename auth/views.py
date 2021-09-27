e197b5ab2bfe291de65e3c5a1a8e5e8991134ea3
VladyslavSkrypets <vladskripets@gmail.com>
2021-09-27 22:51:16 +0300

    add forms for auth

--- auth/views.py
+++ auth/views.py
@@ -1,14 +1,19 @@
-from flask import Blueprint, render_template, make_response
+from __init__ import db
+from flask import Blueprint, render_template, make_response, request
 
 
-auth = Blueprint('auth', __name__, template_folder='templates/auth')
+auth = Blueprint('auth', __name__, template_folder='templates/auth', static_folder='static')
 
 
 @auth.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        pass
     return make_response(render_template("login.html"), 200)
 
 
 @auth.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        pass
     return make_response(render_template("signup.html"), 200)
